a268ad8ec58d195b63e11b037b8b7023fd04fe2d
Dan Kelley <kelley.dan@gmail.com>
2016-01-10 19:31:45 -0400

    levitus

--- create_data/levitus/create_levitus.R
+++ create_data/levitus/create_levitus.R
@@ -1,6 +1,6 @@
 library(oce)
 library(ncdf4)
-con <- nc_open("/data/oar/levitus/temperature_annual_1deg.nc")
+con <- nc_open("temperature_annual_1deg.nc")
 get <- function(f, item) {
     res <- ncvar_get(f, item)
     if (1 == length(dim(res))) {
@@ -17,7 +17,7 @@ longitude <- get(con, "lon")
 latitude <- get(con, "lat")
 SST <- get(con, "t_an")[,,1]
 SST[SST > 100] <- NA
-con <- nc_open("/data/oar/levitus/salinity_annual_1deg.nc")
+con <- nc_open("salinity_annual_1deg.nc")
 SSS <- get(con, "s_an")[,,1]
 SSS[SSS > 100] <- NA
 levitus <- list(longitude=longitude, latitude=latitude, SSS=SSS, SST=SST)
