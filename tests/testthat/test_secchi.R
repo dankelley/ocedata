f1fd7e79795edea7a149916e7a727621b1ef1f03
Dan Kelley <kelley.dan@gmail.com>
2016-01-10 21:29:09 -0400

    test

--- tests/testthat/test_secchi.R
+++ tests/testthat/test_secchi.R
@@ -1,9 +1,8 @@
 ## vim:textwidth=80:expandtab:shiftwidth=4:softtabstop=4
-library("ocedata")
 library("testthat")
 context("secchi")
 test_that("secchi has correct form", {
-          data("secchi")
+          data("secchi", package="ocedata")
           expect_true(is.data.frame(secchi))
           expect_equal(4, sum(c("latitude", "longitude", "depth", "time") %in% names(secchi)))
 })
