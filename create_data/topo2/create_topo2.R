7e282c7a4707201b1e1d7606cb2148b83492c45a
Dan Kelley <kelley.dan@gmail.com>
2016-01-18 07:45:28 -0400

    fix doc link

--- create_data/topo2/create_topo2.R
+++ create_data/topo2/create_topo2.R
@@ -1,6 +1,6 @@
 library(oce)
 data(topoWorld)
-coarse <- decimate(topoWorld, by=4)
+coarse <- oce::decimate(topoWorld, by=4)
 topo2 <- coarse[["z"]]
 save(topo2, file="topo2.rda")
 tools::resaveRdaFiles("topo2.rda")
