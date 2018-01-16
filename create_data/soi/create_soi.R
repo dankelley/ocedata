9cecbe114e3267bfe57177b6894e1ddba7ec1084
dankelley <kelley.dan@gmail.com>
2018-01-16 15:15:44 -0400

    data(soi) updated for newly-acquired data

--- create_data/soi/create_soi.R
+++ create_data/soi/create_soi.R
@@ -1,9 +1,14 @@
 library(ncdf4)
 url <- "http://www.cgd.ucar.edu/cas/catalog/climind/SOI.nc"
-download.file(url, "SOI.nc")
+if (!file.exists("SOI.nc")) {
+    cat("downloading file from", url, "\n")
+    download.file(url, "SOI.nc")
+}
 f <- nc_open("SOI.nc")
 year <- as.vector(ncvar_get(f, "TIME"))
 index <- as.vector(ncvar_get(f, "SOI_SIGNAL"))
+nc_close(f)
+
 ok <- is.finite(index)
 year <- year[ok]
 index <- index[ok]
--- create_data/soi/test_soi.R
+++ create_data/soi/test_soi.R
@@ -1,3 +1,4 @@
 load("soi.rda")
+str(soi)
 par(mar=c(3,3,1,1),mgp=c(2,0.7,0))
 plot(soi$year, soi$index, type='l')
