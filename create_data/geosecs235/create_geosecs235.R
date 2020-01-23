f49a992559030ce40c55f18a384d3339fdfd8569
dankelley <kelley.dan@gmail.com>
2020-01-23 12:51:22 -0400

    replace ctdAddColumn() with oceSetData()

--- create_data/geosecs235/create_geosecs235.R
+++ create_data/geosecs235/create_geosecs235.R
@@ -31,45 +31,48 @@ geosecs235@metadata$deploymentType <- "profile"
 geosecs235@metadata$filename <- "geosecs235.cdf"
 
 ## Add columns, in the order they show up in the netcdf summary.
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"AOU"), name="AOU", label="Apparent oxygen utilization",
-                           unit=list(expression(unit=mu*mol/kg), scale=""), log=FALSE, originalName="AOU")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"BOTTLE"), name="bottle", label="Bottle",
-                           unit=list(unit=expression(), scale=""), log=FALSE, originalName="BOTTLE")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"Delta_C-13"), name="delta13C", label="Delta 13C",
-                           unit=list(unit=expression(per*mil), scale=""), log=FALSE, originalName="Delta_C-13")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"Delta_C-14"), name="delta14C", label="Delta 14C",
-                           unit=list(unit=expression(per*mil), scale=""), log=FALSE, originalName="Delta_C-14")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"DEPTH"), name="depth", label="Depth",
-                           unit=list(unit=expression(m), scale=""), log=FALSE, originalName="DEPTH")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"NO2"), name="nitrite", label="Nitrite",
-                           unit=list(unit=expression(mu*mol/kg), scale=""), log=FALSE, originalName="NO2")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"NO3"), name="nitrate", label="Nitrate",
-                           unit=list(unit=expression(mu*mol/kg), scale=""), log=FALSE, originalName="NO3")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"OXY"), name="oxygen", label="Oxygen",
-                           unit=list(unit=expression(mu*mol/kg), scale=""), log=FALSE, originalName="OXY")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"PCO2"), name="pCO2", label="pCO2",
-                           unit=list(unit=expression(mu*atm), scale=""), log=FALSE, originalName="PCO2")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"PCO2_TEMP"), name="pCO2temperature", label="pCO2temperature",
-                           unit=list(unit=expression(degree*C), scale="ITS-90"), log=FALSE, originalName="PCO2_TEMP")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"SIGMA0"), name="sigma0", label="Sigma0",
-                           unit=list(unit=expression(kg/m^3), scale=""), log=FALSE, originalName="SIGMA0")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"SIGMA2"), name="sigma2", label="Sigma2",
-                           unit=list(unit=expression(kg/m^3), scale=""), log=FALSE, originalName="SIGMA2")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"SIGMA4"), name="sigma4", label="Sigma4",
-                           unit=list(unit=expression(kg/m^3), scale=""), log=FALSE, originalName="SIGMA4")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"SIO3"), name="silicate", label="Silicate",
-                           unit=list(unit=expression(mu*mol/kg), scale=""), log=FALSE, originalName="SIO3")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"TALK"), name="totalAlkalinity", label="TotalAlkalinity",
-                           unit=list(unit=expression(mu*equiv/kg), scale=""), log=FALSE, originalName="TALK")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"THETA"), name="theta", label="theta",
-                           unit=list(unit=expression(degree*C), scale="ITS-90"), log=FALSE, originalName="THETA")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"TCO2"), name="totalCO2", label="totalCO2",
-                           unit=list(unit=expression(mu*mol/kg), scale=""), log=FALSE, originalName="TCO2")
-geosecs235 <- ctdAddColumn(geosecs235, column=get(f,"Tritium_TU"), name="tritium", label="Tritium",
-                           unit=list(unit=expression(TU), scale=""), log=FALSE, originalName="Tritium_TU")
-summary(geosecs235)
+# object name value unit originalName note
+geosecs235 <- oceSetData(geosecs235, value=get(f,"AOU"), name="AOU",
+                         unit=list(unit=expression(mu*mol/kg), scale=""), originalName="AOU")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"AOU"), name="AOU",
+                         unit=list(unit=expression(unit=mu*mol/kg), scale=""), originalName="AOU")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"BOTTLE"), name="bottle",
+                         unit=list(unit=expression(), scale=""), originalName="BOTTLE")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"Delta_C-13"), name="delta13C",
+                         unit=list(unit=expression(per*mil), scale=""), originalName="Delta_C-13")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"Delta_C-14"), name="delta14C",
+                         unit=list(unit=expression(per*mil), scale=""), originalName="Delta_C-14")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"DEPTH"), name="depth",
+                         unit=list(unit=expression(m), scale=""), originalName="DEPTH")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"NO2"), name="nitrite",
+                         unit=list(unit=expression(mu*mol/kg), scale=""), originalName="NO2")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"NO3"), name="nitrate",
+                         unit=list(unit=expression(mu*mol/kg), scale=""), originalName="NO3")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"OXY"), name="oxygen",
+                         unit=list(unit=expression(mu*mol/kg), scale=""), originalName="OXY")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"PCO2"), name="pCO2",
+                         unit=list(unit=expression(mu*atm), scale=""), originalName="PCO2")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"PCO2_TEMP"), name="pCO2temperature",
+                         unit=list(unit=expression(degree*C), scale="ITS-90"), originalName="PCO2_TEMP")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"SIGMA0"), name="sigma0",
+                         unit=list(unit=expression(kg/m^3), scale=""), originalName="SIGMA0")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"SIGMA2"), name="sigma2",
+                         unit=list(unit=expression(kg/m^3), scale=""), originalName="SIGMA2")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"SIGMA4"), name="sigma4",
+                         unit=list(unit=expression(kg/m^3), scale=""), originalName="SIGMA4")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"SIO3"), name="silicate",
+                         unit=list(unit=expression(mu*mol/kg), scale=""), originalName="SIO3")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"TALK"), name="totalAlkalinity",
+                         unit=list(unit=expression(mu*equiv/kg), scale=""), originalName="TALK")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"THETA"), name="theta",
+                         unit=list(unit=expression(degree*C), scale="ITS-90"), originalName="THETA")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"TCO2"), name="totalCO2",
+                         unit=list(unit=expression(mu*mol/kg), scale=""), originalName="TCO2")
+geosecs235 <- oceSetData(geosecs235, value=get(f,"Tritium_TU"), name="tritium",
+                         unit=list(unit=expression(TU), scale=""), originalName="Tritium_TU")
 save(geosecs235, file="geosecs235.rda")
 tools::resaveRdaFiles("geosecs235.rda")
 
-plotProfile(geosecs235, xtype="tritium", type='p', ylim=c(600, 0))
+### summary(geosecs235)
+### plotProfile(geosecs235, xtype="tritium", type='p', ylim=c(600, 0))
 
