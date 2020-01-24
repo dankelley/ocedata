fc5d9d4ffb5eeb6b501d5f140c48f7519eb17062
dankelley <kelley.dan@gmail.com>
2020-01-24 08:37:42 -0400

    coastline*: R wrappers to avoid oce dependence

---
+++ data/coastlineWorldMedium.R
@@ -0,0 +1,13 @@
+stopifnot(requireNamespace("methods", quietly=TRUE),
+          requireNamespace("ocedata", quietly=TRUE))
+
+coastlineWorldMedium <-
+    local({
+        load(system.file(file.path("extdata", "coastlineWorldMedium_slots.rda"), package="ocedata"))
+        res <- new("oce")
+        slot(res, "data") <- data
+        slot(res, "metadata") <- metadata
+        slot(res, "processingLog") <- processingLog
+        res
+    })
+
