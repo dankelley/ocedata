449a8b889cbf732e9e02884acc1e8b7e44c2f8a2
dankelley <kelley.dan@gmail.com>
2022-04-07 17:07:11 -0300

    fix https://github.com/dankelley/ocedata/issues/19

---
+++ R/ocedata.R
@@ -0,0 +1,19 @@
+# vim:textwidth=80:expandtab:shiftwidth=4:softtabstop=4
+
+#' ocedata: Datasets for the oce package
+#'
+#' @description
+#' The ocedata package provides datasets for use in oceanographic analysis.
+#'
+#' @details
+#' Some commonly-used oceanographic datasets are too large to be stored within
+#' an analysis package such as \CRANpkg{oce}, or are of sufficient general
+#' interest to merit inclusion in a separate package.  Such datasets are
+#' provided here, for use by Oceanographers using the \CRANpkg{oce} package or
+#' other more general packages.
+#'
+#' @docType package
+#'
+#' @name ocedata
+NULL
+
